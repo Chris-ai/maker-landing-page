a96c485636b1429dc64d60166c798bdde2772591
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-02-16 17:45:39 +0100

    feat(notifications): add form validation

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-function logSubmit(event) {
-  
-  event.preventDefault();
-  	const data = new FormData(event.target);
-	const {email} = Object.fromEntries(data.entries());
-	console.log(email);
-
-	if(email === '') {
-		console.log('empty!')
-	} else if (!validateEmail(email)){
-		console.log('does not look like an email')
-	} else {
-		console.log('success!')
-	}
-}
-
 const form = document.getElementById("notification-form");
-
-console.log(form, '<- form');
-
-form.addEventListener("submit", logSubmit);
-
+const toast = document.getElementById("toast");
+const errorMessageContainer = document.getElementById("error-message");
+const emailInput = document.getElementById("email-input");
 
 const validateEmail = (email) => {
-	return String(email)
+  return String(email)
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
+};
+
+const logSuccess = () => {
+  toast.classList.add("show-toast");
+  setTimeout(() => {
+    toast.classList.remove("show-toast");
+  }, 2000);
+};
+
+const showError = () => {
+  emailInput.classList.add("input-error");
+  errorMessageContainer.classList.add("showErrorMessage");
+};
+
+const sendNotification = () => {
+  errorMessageContainer.classList.remove("showErrorMessage");
+  emailInput.classList.remove("input-error");
+  emailInput.value = "";
+  logSuccess();
+};
+
+function logSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const { email } = Object.fromEntries(data.entries());
+
+  if (!validateEmail(email)) {
+    showError();
+  } else if (email) {
+    sendNotification();
+  }
 }
+
+form.addEventListener("submit", logSubmit);
